e47cb7efde2905a0e1cfbdc7822a3d8d85d1d6e8
xdemocle <xdemocle@gmail.com>
2022-08-21 12:36:20 +0200

    fix tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app';
+import DictionaryProvider from './context/dictionary';
+import TeamProvider from './context/teams';
 
 test('renders Unlock to text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Unlock to/i);
+  render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <DictionaryProvider>
+          <TeamProvider>
+            <App />
+          </TeamProvider>
+        </DictionaryProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+  const linkElement = screen.getByText(/Xceed Liga Challenge 2022/i);
   expect(linkElement).toBeInTheDocument();
 });
